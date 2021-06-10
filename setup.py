fe563e306c891bc154a116ae68c9fa07b2cac04e
Srikar Reddy Karemma <srikar_reddy@Srikars-MacBook-Pro.local>
2021-06-10 23:27:44 +0530

    ksr14

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import setup, find_packages
 #install_requires = ["matplotlib", "hapiclient @ git+https://github.com/hapi-server/client-python#egg=hapiclient"]
 install_requires = ["matplotlib", "hapiclient>0.1.7"]
 
-if sys.argv[1] == 'develop':
-    install_requires.append("deepdiff<3.3.0")
-    if sys.version_info < (3, 6):
-        install_requires.append("pytest<5.0.0")
-    else:
-        # Should not be needed, as per
-        # https://docs.pytest.org/en/stable/py27-py34-deprecation.html
-        # Perhaps old version of pip causes this?
-        install_requires.append("pytest")
+# if sys.argv[1] == 'develop':
+#     install_requires.append("deepdiff<3.3.0")
+#     if sys.version_info < (3, 6):
+#         install_requires.append("pytest<5.0.0")
+#     else:
+#         # Should not be needed, as per
+#         # https://docs.pytest.org/en/stable/py27-py34-deprecation.html
+#         # Perhaps old version of pip causes this?
+#         install_requires.append("pytest")
 
 # version is modified by misc/version.py (executed from Makefile). Do not edit.
 setup(
